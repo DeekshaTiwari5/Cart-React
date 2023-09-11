bebe7f170216179a802507eb87b454df2c82a01e
unknown <tdeeksha617@gmail.com>
2023-09-11 16:37:03 +0530

    add increasequantity function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img: ""
+
+        }
+       // this.increaseQuantity = this.increaseQuantity.bind(this);//2 nd way
+    }
+    increaseQuantity=() => {          // 3rd way od binding arrow functions
+         console.log('this.state',this.state); 
+    }
     render () {
+        const { price, title, qty } = this.state; // destructuring
         return (
          <div className='cart-item'>
             <div className='left-block'>
@@ -9,11 +24,32 @@ class CartItem extends React.Component {
 
             </div>
             <div className='right-block'>
-                <div style={{fontSize: 25}}>Phone</div>
-                <div style={{color: '#777'}}>Rs. 999</div>
-                <div style={{color: '#777'}}>Qty: 1</div> 
+            <div style={{fontSize: 25}}>{title}</div>
+            <div style={{color: '#777'}}> Rs.{price}</div>
+            <div style={{color: '#777'}}> Qty:{qty}</div>
+               {/*<div style={{fontSize: 25}}>{this.state.title}</div> <div style={{fontSize: 25}}>Phone</div>*/}
+               {/*<div style={{color: '#777'}}>{this.state.price}</div>           {/*<div style={{color: '#777'}}>Rs. 999</div>*/}
+               {/*<div style={{color: '#777'}}>{this.state.qty}</div>            {/*<div style={{color: '#777'}}>Qty: 1</div> */}
                 <div className='cart-item-actions'>
-                {/* Buttons*/}
+                {/* Buttons :flaticon*/}
+
+                <img 
+                  alt='increase' 
+                  className='action-icons' 
+                  src='https://cdn-icons-png.flaticon.com/128/992/992651.png'
+                  onClick={this.increaseQuantity.bind(this)}    //onClick={this.increaseQuantity.bind(this)} //1 st way od binding
+                /> 
+                <img 
+                  alt='decrease' 
+                  className='action-icons' 
+                  src='https://t3.ftcdn.net/jpg/05/25/82/62/240_F_525826235_kQXRqPCQyDuhXGjV4FjpVY8Mb68jGJD8.jpg'
+                />
+                <img 
+                  alt='delete' 
+                  className='action-icons' 
+                  src='https://cdn-icons-png.flaticon.com/128/3405/3405244.png'
+                />
+                
                 </div>                   
             </div>
          </div>
