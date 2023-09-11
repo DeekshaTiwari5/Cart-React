de4fedb472574178d5f13d13edc78c1cefe10141
unknown <tdeeksha617@gmail.com>
2023-09-11 20:29:01 +0530

    modify State

--- src/CartItem.js
+++ src/CartItem.js
@@ -6,14 +6,35 @@ class CartItem extends React.Component {
         this.state = {
             price: 999,
             title: 'Mobile Phone',
-            qty: 1,
+            qty: 1, // shallow merging : react will change only qty in the state and not change other properties
             img: ""
 
         }
        // this.increaseQuantity = this.increaseQuantity.bind(this);//2 nd way
     }
     increaseQuantity=() => {          // 3rd way od binding arrow functions
-         console.log('this.state',this.state); 
+        //  this.state.qty += 1;
+        //  console.log('this.state',this.state); 
+// ----------------- setState form 1 : if require function form
+//         this.setState({
+//             qty: this.state.qty + 1
+//             title : "some new title"
+//         });
+
+//----------------- setState form  2 : if require prevState  use this
+        this.setState((prevState) => {
+            return{ 
+            qty: prevState.qty + 1
+            }
+        });
+    }
+    decreaseQuantity=() => {
+        this.setState((prevState) => {
+            return{ 
+            qty: prevState.qty - 1
+            }
+        });
+
     }
     render () {
         const { price, title, qty } = this.state; // destructuring
@@ -43,6 +64,7 @@ class CartItem extends React.Component {
                   alt='decrease' 
                   className='action-icons' 
                   src='https://t3.ftcdn.net/jpg/05/25/82/62/240_F_525826235_kQXRqPCQyDuhXGjV4FjpVY8Mb68jGJD8.jpg'
+                  onClick={this.decreaseQuantity.bind(this)} 
                 />
                 <img 
                   alt='delete' 
